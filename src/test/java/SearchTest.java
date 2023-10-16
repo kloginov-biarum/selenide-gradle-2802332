c930e2e8949ce01ffb5fc49c04fee3777d1381cd
kloginov-biarum <kloginov@biarum.com>
2023-10-16 12:23:07 +0200

    add

--- src/test/java/SearchTest.java
+++ src/test/java/SearchTest.java
@@ -2,21 +2,39 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byId;
 import static com.codeborne.selenide.Selenide.*;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class SearchTest {
 
-    @Test
-    public void validSearch(){
+    //Java
+    //Python
+    //bicycle
+
+    @ParameterizedTest
+    @MethodSource("validData")
+    public void validSearch(String query){
         open("https://duckduckgo.com/");
-        $(byId("searchbox_input")).shouldBe(visible).setValue("Java");
+        $(byId("searchbox_input")).shouldBe(visible).setValue(query);
         $("[class*=\"searchbox_input\"]").click();
-        ElementsCollection results = $$("[data-testid=\"result\"]");
-        for (SelenideElement result: results) {
-            result.shouldHave(text("Java"));
+        ElementsCollection queryResults = $$("[data-testid=\"result\"]");
+        for (SelenideElement result: queryResults) {
+            result.shouldHave(text(query));
         }
+    }
+    static Stream<Arguments> validData () {
+        return Stream.of(
+                arguments("java"),
+                arguments("python"),
+                arguments("bicycle")
+        );
     }
 }
